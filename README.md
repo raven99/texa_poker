11d322b5c78828ddb0acbad93fcaaf005687ebaa
raven99 <740517444@qq.com>
2015-06-02 11:32:13 +0800

    add a simple method: fold when the card is not promising

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # texa_poker
-a game for huawei soft ware contest
+a game for huawei soft-ware contest
+
+2015/05/30 离比赛上交程序没几天了, 现在还只能allin,最近两天赶紧把比牌的逻辑做出来,然后做一个简单的策略好了
+
+
--- game_cpp.cpp
+++ game_cpp.cpp
@@ -4,8 +4,12 @@
 #include <string>
 #include <sstream>
 #include <stdlib.h>
+#include <algorithm>
 
 using namespace std;
+bool flush_psb(const string &);
+bool high_card(const string &);
+
 int main ( int argc, char* argv[] )
 {
     if(argc != 6) 
@@ -28,6 +32,9 @@ int main ( int argc, char* argv[] )
     ss << "reg: " << my_id << " Killer\n";
     reply = ss.str();
     client_socket << reply;
+
+    int flag = 0; //the deal method
+
     while(true)
     { 
         client_socket >> reply;
@@ -36,12 +43,44 @@ int main ( int argc, char* argv[] )
             cout << "game is over" << endl;
             break;
         }
-        if(reply.find("hold") != string::npos)
+        //两张底牌信息处理
+        if(reply.find("hold/") && reply.find("/hold"))
         {
-             
+             //test if there is a complete hold message 
+            if(flush_psb(reply))
+                flag = 1;
+            else if(high_card(reply))
+                flag = 1;
         }
         if(reply.find("inquire") != string::npos)
-            client_socket << "all_in\n";
+        {  
+            if(flag)
+            client_socket << "check\n";
+            else
+            client_socket << "fold\n";
+        }
     }
   return 0;
+}
+
+bool flush_psb(const string &flush )
+{
+    if(flush.find("SPADES") != flush.rfind("SPADES"))
+        return true;
+    else if(flush.find("HEARTS") != flush.rfind("HEARTS"))
+        return true;
+    else if(flush.find("CLUBS") != flush.rfind("CLUBS"))
+        return true;
+    else if(flush.find("DIAMONDS") != flush.rfind("DIAMONDS"))
+        return true;
+    else 
+        return false;
+}
+
+bool high_card(const string &high)
+{
+    if(high.find("K")||high.find("Q")||high.find("J")||high.find("A"))
+        return true;
+    else
+        return false;
 }
